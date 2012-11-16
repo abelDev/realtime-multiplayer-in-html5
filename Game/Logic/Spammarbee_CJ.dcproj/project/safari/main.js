9ca19d09853380569396e4038dea15dfe05bc00e
Rodrigo Villacorta <rodrigo.villacorta1@gmail.com>
2012-11-15 20:20:02 -0500

    Game Logic Update 0.02
    Changelist:
    Added Scoreboard
    Added Custom Team support
    Added Score Keeper
    Allowed loading Main without debug.

--- Game/Logic/Spammarbee_CJ.dcproj/project/safari/main.js
+++ Game/Logic/Spammarbee_CJ.dcproj/project/safari/main.js
@@ -27,12 +27,23 @@ document.getElementById("stackLayout").object.setCurrentView("Team2", false);
 
 function BeginGame(event)
 {
-document.getElementById("stackLayout").object.setCurrentView("Ready", false);
+document.getElementById("stackLayout").object.setCurrentView("Scoreboard", false);
+WriteScores();
+}
+
+function LoadQuestion1(event)
+{
+document.getElementById("stackLayout").object.setCurrentView("Main", false);
 }
 
 function PickCstTeam1(event)
 {
 document.getElementById("TeamPicker").object.setCurrentView("CustomTeam1", false);
+}
+
+function PickCstTeam2(event)
+{
+document.getElementById("TeamPicker1").object.setCurrentView("view2", false);
 }
 
 function LoadMain(event)
@@ -52,6 +63,8 @@ window.location.reload()
 
 var TeamName1;
 var TeamPoints1;
+var TeamName2;
+var TeamPoints2;
 
 function setupLogic()
 {
@@ -79,7 +92,47 @@ function HomelessSetup()
     TeamPoints1=0;
     document.getElementById("TeamStringUI").innerText = TeamName1;
     document.getElementById("salpick1").object.setEnabled(false);
-    document.getElementById("salpick2").object.setEnabled(false);
+    document.getElementById("homepick2").object.setEnabled(false);
     document.getElementById("greenpick1").object.setEnabled(false);
     document.getElementById("custom1").object.setEnabled(false);
+}
+
+function CustomSetup()
+{
+    var textBox1 = document.getElementById("CustNameField");
+    var CustName1 = textBox1.value;
+    TeamName1 = CustName1;
+    TeamPoints1 = 0;
+    document.getElementById("TeamStringUI").innerText = TeamName1;
+    document.getElementById("custompick1").object.setText("Picked");
+    document.getElementById("salpick1").object.setEnabled(false);
+    document.getElementById("greenpick1").object.setEnabled(false);
+    document.getElementById("custom1").object.setEnabled(false);
+    document.getElementById("homepick1").object.setEnabled(false);
+}
+
+function CustomSetup2()
+{
+    var textBox2 = document.getElementById("CustNameField2");
+    var CustName2 = textBox2.value;
+    TeamName2 = CustName2;
+    TeamPoints2 = 0;
+    document.getElementById("custompick2").object.setText("Picked");
+    document.getElementById("salpick2").object.setEnabled(false);
+    document.getElementById("greenpick2").object.setEnabled(false);
+    document.getElementById("custom2").object.setEnabled(false);
+    document.getElementById("homepick2").object.setEnabled(false);
+}
+
+//
+// Scoreboard.js
+// Keeps Score
+//
+
+function WriteScores()
+{
+    document.getElementById("Team1Scr").innerText = TeamName1;
+    document.getElementById("Team2Scr").innerText = TeamName2;
+    document.getElementById("Team1Num").innerText = TeamPoints1;
+    document.getElementById("Team2Num").innerText = TeamPoints2;
 }
